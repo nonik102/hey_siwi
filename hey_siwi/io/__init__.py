4e771a16d3cf2610ce1ed31096a8e4c8cb2cf7cd
Noah McCarthy <nonik102@gmail.com>
2025-05-30 23:00:44 -0400

    test cmd

--- hey_siwi/io/__init__.py
+++ hey_siwi/io/__init__.py
@@ -12,15 +12,40 @@ class Monitor:
 
         self._buffer: list[str] = []
 
+    def __str__(self) -> str:
+        return "\n".join(self._buffer)
+
     def set_screen(self, path: Path) -> None:
+        # NOTE: we could speed this up by doing validation in the read-in
         try:
             with open(path, 'r') as fp:
-                self._buffer = fp.readlines()
+                self._buffer = [
+                    line.strip('\n') for line in fp.readlines()
+                ]
         except IOError as ex:
             raise MonitorError("Failed to read file") from ex
 
+        # validation
+        if max(len(l) for l in self._buffer) > self.width:
+            raise MonitorError("Line is too long")
+        if len(self._buffer) > self.length:
+            raise MonitorError("File is too long")
+
+    def print_screen(self) -> None:
+        print(self)
 
 @click.group()
 def show():
-    pass
+    ...
+
+@show.command()
+def img():
+    test_path = Path('/home/nonik/development/hey_siwi/test')
+    monitor = Monitor(10, 10)
+    monitor.set_screen(test_path)
+    monitor.print_screen()
+
+    return 0
+
+
 
